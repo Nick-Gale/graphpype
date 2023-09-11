6f5de986a303644e0f4eac0c57f6e1880b44e59d
Nick Gale <nick.gale1993@gmail.com>
2023-09-11 13:19:09 +0800

    .

--- src/submodule_analysis.py
+++ src/submodule_analysis.py
@@ -1,4 +1,6 @@
 # Module: Analysis functions. All graph theoretical, statistical, and other analyses live here.
+"""Functions that are used in the analysis of a processed graph. These can 
+
 import networkx as netx
 import 
 # Constants
--- src/submodule_statistics.py
+++ src/submodule_statistics.py
@@ -2,6 +2,7 @@ import stan as st
 import scipy.stats as stat
 import numpy as np
 import itertools as it
+import statsmodels.api as sm
 
 """A series of utility functions related to computing various useful statistical measures. These functions assume data in the form of N x N x S tensors where the first and second indexes are the same dimension and the final dimension stores the samples."""
 class ComparisonSet:
@@ -44,6 +45,13 @@ def _power(pvals, nobs, test='ttest')
 #--------------------------------------------
 # The data are in the form of graphs and can be split into control-test groups. A particular statistic vector is calculated as a function of the graph (e.g. average degree, or ROI corellation) across the dataset yielding a distribution of statistics. These can be compared between two groups to identify significant differences in a range of frequentists tests which can be assessed for statistical siginifance (Type I errors) and statistical power (Type II errors).
 
+def glm(X, Y):
+    """The General Linear Model regresses predictors X onto data Y and can be used as a generalisation of a broad class of statistical tests. It assumes errors are normally distributed; for a more general model where errors are assumed to be in the exponential family see Generalised Linear Model. Returns the fit alongside t-test and f-test results."""
+    model = sm.OLS(Y, X)
+    result = model.fit()
+
+    return result.params, result.t_test, result_f_test
+
 # def oneway_anova()
 
 # def pairwise_binomial()
@@ -68,5 +76,23 @@ def spatial_alignment(map1, map2, measure, nsamples, siglevel):
     # pval = dist.cdf(measure(map2))
 
     # return pval < siglevel, pval
+
+
+#--------------------------------------------
+# Classifiers and Regressors
+#--------------------------------------------
+
+class TrainingProtocol:
+
+    def __init__(self):
+
+
+def _ml(dataset, permutation, split, classification_targets, model, training_protocol, seed=1):
+    """Internal function to train a generic classifier. Generates a permutation of the data which is then split into test and training sets. The model is assumed to be an object with all hyperparameters encoded e.g. a neural network with predefined layers """
+    
+
+
+    return model, model_validation_outputs
+
 
 
