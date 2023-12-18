510bde812232b66f18cae0480876e42381c0a76d
Nick Gale <nick.gale1993@gmail.com>
2023-12-18 12:51:33 +0800

    created installation routine

--- editableInstall.sh
+++ editableInstall.sh
@@ -3,6 +3,6 @@
 python3 -m venv venv
 source ./venv/bin/activate
 
-wget https://www.github.com/Nick-Gale/NetworkAnalysis/releases/v001.tar.gz
+wget https://www.github.com/Nick-Gale/NetworkAnalysis/dist/v0/graphpype-v0.0.1.tar.gz
 
 python3 -m install -e v001.tar.gz
