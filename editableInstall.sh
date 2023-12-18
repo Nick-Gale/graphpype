d4435a21b0edbeb2e819b172c03e73435ebad8a4
Nick Gale <nick.gale1993@gmail.com>
2023-12-18 14:09:36 +0800

    Changed install grab to curl instead of wget for Mac compatibility

--- editableInstall.sh
+++ editableInstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Create a simple development install for the graphpype neural imaging package
 python3 -m venv venv
-wget -O install.tar.gz https://github.com/Nick-Gale/NetworkAnalysis/raw/main/dist/v0/graphpype-v0.tar.gz 
-tar -xzvf install.tar.gz
-rm install.tar.gz
+curl -LJO https://github.com/Nick-Gale/NetworkAnalysis/raw/main/dist/v0/graphpype-v0.tar.gz
+tar -xzvf graphpype-v0.tar.gz
+rm graphpype-v0.tar.gz
 source ./venv/bin/activate
 python3 -m pip install -e .
