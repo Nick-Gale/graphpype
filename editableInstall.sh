471f6301369d92955a2741f2a536e064ddd093f3
Nick Gale <nick.gale1993@gmail.com>
2023-12-18 13:00:49 +0800

    Install script

--- editableInstall.sh
+++ editableInstall.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
+# Create a simple development install for the graphpype neural imaging package
 python3 -m venv venv
 source ./venv/bin/activate
-
 wget https://www.github.com/Nick-Gale/NetworkAnalysis/dist/v0/graphpype-v0.0.1.tar.gz
-
-python3 -m install -e v001.tar.gz
+tar -xzvf graphpype-v0.0.1.tar.gz
+python3 -m install -e .
